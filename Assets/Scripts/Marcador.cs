45b7981ddc7f51ff5e8559af16335529a67e6afa
izarorubio <izaro.rubio@euneiz.com>
2025-03-25 12:09:41 +0100

    Marcador con Final

--- Assets/Scripts/Marcador.cs
+++ Assets/Scripts/Marcador.cs
@@ -6,11 +6,18 @@ public class Marcador : MonoBehaviour
 {
     public TMP_Text puntosText;
     private int puntos = 0;
+    private int totalPuntos = 5;
 
     // Sumar puntos en el marcador
     public void SumarPuntos()
     {
         puntos++;
         puntosText.text = puntos.ToString(); // Actualizar el texto de los puntos
+
+        // Si llega al total de puntos (nº de paredes)...
+        if (puntos == totalPuntos)
+        {
+            puntosText.text = "FIN";
+        }
     }
 }
